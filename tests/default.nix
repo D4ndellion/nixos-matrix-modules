f426b65172d79c849606cfc4d4a2cbb375c3e3b9
h7x4 <h7x4@nani.wtf>
2026-06-22 15:17:26 +0900

    tests/synapse: init
    This commit ports a part of the integration test from nixpkgs to be used for
    the matrix-synapse-next module.

--- tests/default.nix
+++ tests/default.nix
@@ -1,4 +1,6 @@
 { nixpkgs, pkgs, matrix-lib, ... }:
 {
   nginx-pipeline-eval = pkgs.callPackage ./nginx-pipeline { inherit nixpkgs matrix-lib; };
+
+  synapse = pkgs.testers.runNixOSTest ./synapse;
 }
